806fa5953ba81402617b90d41f6c725c2facc098
kevgllt <kevingullytps@gmail.com>
2025-09-06 21:39:21 -0300

    Add tabs and storages permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    // Adicione a permissão aqui:
+    permissions: [
+      "tabs",
+      "storage" // Boa ideia manter, vamos precisar dela para salvar o ID da aba de música.
+    ],
+    // ... outras configurações do manifesto ...
+  },
 });
